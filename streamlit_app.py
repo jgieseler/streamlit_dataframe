5baa5ffbebba2f78101575a3592b143955e6710a
Jan Gieseler <39489154+jgieseler@users.noreply.github.com>
2025-10-10 16:10:41 +0300

    Move to AgGrid

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from st_aggrid import AgGrid
 
 st.set_page_config(
     page_title="SERPENTINE Solar Cycle 25 SEP Events Catalog",
@@ -15,6 +16,18 @@ st.set_page_config(
 
 df = pd.read_csv('sep-sc25.csv', sep=';')
 
-st.dataframe(df, height=700)
+sc = {}
+sc['BepiC'] = st.checkbox("BepiColombo", value=True)
+sc['L1'] = st.checkbox("L1 (SOHO/Wind)", value=True)
+sc['PSP'] = st.checkbox("Parker Solar Probe", value=True)
+sc['STA'] = st.checkbox("STEREO A", value=True)
+sc['SolO'] = st.checkbox("Solar Orbiter", value=True)
 
-st.markdown("See [this documentation](https://docs.streamlit.io/develop/concepts/design/dataframes#stdataframe-ui-features) for what can be done with the table above.")
+for key, value in sc.items():
+    if not value:
+        df.drop(df.filter(like=f'{key}_',axis=1).columns.to_list(), axis=1, inplace=True)
+
+AgGrid(df)
+
+# st.dataframe(df, height=700)
+# st.markdown("See [this documentation](https://docs.streamlit.io/develop/concepts/design/dataframes#stdataframe-ui-features) for what can be done with the table above.")
