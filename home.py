f0474bc2d7a9c6311cf4793fe052b048760b0f85
Jan Gieseler <jan.gieseler@utu.fi>
2025-10-15 16:42:07 +0300

    Add acknowledgement section with funding information and logos

--- home.py
+++ home.py
@@ -8,3 +8,14 @@ st.write('Here you find some general info about the catalogues and how to use th
 st.write('Select the catalogue you want to explore from the top navigation bar.')
 
 st.write('Open the sidebar (">>" in the top left) for options.')
+
+st.markdown("""
+            ### Acknowledgement
+
+            <img align="right" height="80px" src="https://github.com/user-attachments/assets/28c60e00-85b4-4cf3-a422-6f0524c42234" alt="EU flag">
+            <img align="right" height="80px" src="https://github.com/user-attachments/assets/5bec543a-5d80-4083-9357-f11bc4b339bd" alt="SOLER logo">
+
+            These catalogues are developed within the SOLER (*Energetic Solar Eruptions: Data and Analysis Tools*) project. SOLER has received funding from the European Union’s Horizon Europe programme under grant agreement No 101134999.
+
+            The catalogues reflects only the authors’ view and the European Commission is not responsible for any use that may be made of the information it contains.
+            """, unsafe_allow_html=True)
