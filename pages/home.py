9de2355bd10a831d4e993f835363d8c11cd212af
Jan Gieseler <jan.gieseler@utu.fi>
2025-11-14 11:21:11 +0200

    Fix image alignment in acknowledgements section of home.py

--- pages/home.py
+++ pages/home.py
@@ -9,10 +9,11 @@ st.write('Select the catalogue you want to explore from the top navigation bar.'
 
 st.write('Open the sidebar (">>" in the top left) for options.')
 
+
 st.markdown("""
             #### Acknowledgement
 
-            <img align="right" height="80px" src="https://github.com/user-attachments/assets/28c60e00-85b4-4cf3-a422-6f0524c42234" alt="EU flag">
+            <img hspace="10px" align="right" height="80px" src="https://github.com/user-attachments/assets/28c60e00-85b4-4cf3-a422-6f0524c42234" alt="EU flag">
             <img align="right" height="80px" src="https://github.com/user-attachments/assets/5bec543a-5d80-4083-9357-f11bc4b339bd" alt="SOLER logo">
 
             These catalogues are developed within the SOLER (*Energetic Solar Eruptions: Data and Analysis Tools*) project. SOLER has received funding from the European Union’s Horizon Europe programme under grant agreement No 101134999.
